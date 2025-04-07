fa01014e41c88e0071840052d733da48d1e32d9a
Matheus <matheusdasilvagastaldi@gmail.com>
2025-04-06 22:18:59 -0300

    Corrigindo TaskData

--- app/src/main/java/br/edu/satc/todolistcompose/MainActivity.kt
+++ app/src/main/java/br/edu/satc/todolistcompose/MainActivity.kt
@@ -10,12 +10,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
 import br.edu.satc.todolistcompose.ui.screens.HomeScreen
 import br.edu.satc.todolistcompose.ui.theme.ToDoListComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "satc-demo-room"
+        ).allowMainThreadQueries().build()
+
+        val taskDao = db.taskDao()
+
+        if (taskDao.getAll().isEmpty()) {
+            val task1 = TaskData(0, "Estudar Kotlin", "Revisar funções e listas", false)
+            val task2 = TaskData(0, "Fazer projeto Room", "Implementar DAO e mostrar no app", false)
+
+            taskDao.insertAll(task1, task2)
+        }
 
         setContent {
             ToDoListComposeTheme {
@@ -28,21 +44,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToDoListComposeTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/br/edu/satc/todolistcompose/TaskData.kt
+++ app/src/main/java/br/edu/satc/todolistcompose/TaskData.kt
@@ -2,14 +2,16 @@ package br.edu.satc.todolistcompose
 
 import androidx.room.ColumnInfo
 import androidx.room.Dao
+import androidx.room.Database
 import androidx.room.Delete
 import androidx.room.Entity
 import androidx.room.Insert
 import androidx.room.PrimaryKey
 import androidx.room.Query
+import androidx.room.RoomDatabase
 import androidx.room.Update
 
-@Entity
+@Entity(tableName = "task")
 data class TaskData (
     @PrimaryKey(autoGenerate = true) val uid: Int,
     @ColumnInfo(name = "task_title") val title: String,
@@ -20,20 +22,26 @@ data class TaskData (
 @Dao
 interface TaskDao {
     @Query("SELECT * FROM task")
-    fun getAll(): List<Task>
+    fun getAll(): List<TaskData>
 
     @Query("SELECT * FROM task WHERE uid IN (:taskIds)")
-    fun loadAllByIds(userIds: IntArray): List<Task>
+    fun loadAllByIds(taskIds: IntArray): List<TaskData>
 
     @Query("SELECT * FROM task WHERE task_title LIKE :first")
-    fun findByTitle(first: String, last: String): Task
+    fun findByTitle(first: String, last: String): TaskData
 
     @Update
-    fun updateAll(vararg users: Task)
+    fun updateAll(vararg tasks: TaskData)
 
     @Insert
-    fun insertAll(vararg users: User)
+    fun insertAll(vararg tasks: TaskData)
 
     @Delete
-    fun delete(task: Task)
+    fun delete(task: TaskData)
+}
+
+
+@Database(entities = arrayOf(TaskData::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
 }
